76b83f85fd644b10c3aa22feaf4e9524cd3e807d
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-07-02 16:04:25 -0500

    Add google drive filestream

--- PkgFunctions-normal.ps1
+++ PkgFunctions-normal.ps1
@@ -956,7 +956,21 @@ Function mod-gotomeeting ($obj) {
 }
 
 
+Function mod-google-drive-file-stream ($obj) {
+	$fullurl32 = ($obj.installScriptOrig -split "`n" | Select-String -pattern '^\$url = ').tostring()
+	$url32 = ($fullurl32 -split "'" | Select-String -Pattern "http").ToString()
+	$filename32 = ($url32 -split "/" | Select-Object -Last 1).tostring()
+	$filePath32 = 'file          = (Join-Path $toolsDir "' + $filename32 + '")'
 
+	$obj.installScriptMod = '$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"' + "`n" + $obj.InstallScriptMod
+	$obj.installScriptMod = $obj.installScriptMod -replace "Install-ChocolateyPackage" , "Install-ChocolateyInstallPackage"
+	$obj.installScriptMod = $obj.installScriptMod -replace "$packageArgs = @{" , "$&`n  $filePath32"
+	$exeRemoveString = "`n" + 'Get-ChildItem $toolsDir\*.exe | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }'
+	$obj.installScriptMod = $obj.installScriptMod + $exeRemoveString
+	
+	
+	download-fileSingle -url $url32 -filename $filename32 -toolsDir $obj.toolsDir
+}
 
 
 
