936cb11b4af4be128b03bb1cb0ef16666505f86d
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-08-20 19:30:51 -0500

    Fix nordvpn

--- PkgFunctions-normal.ps1
+++ PkgFunctions-normal.ps1
@@ -390,18 +390,15 @@ Function mod-uplay ($obj) {
 
 
 Function mod-nordvpn ($obj) {
-
-	$version = $obj.version
-
-	$url32 = "https://downloads.nordcdn.com/apps/windows/10/NordVPN/" + $version + "/NordVPNSetup.exe"
-	$filename32 = "NordVPNSetup.exe"
-
+    $fullurl32  = ($obj.installScriptOrig -split "`n" | Select-String -pattern ' url  ').tostring()
+	$url32      = ($fullurl32 -split "'" | Select-String -Pattern "http").tostring()
+	$filename32 = ($url32 -split "/" | Select-Object -Last 1).tostring()
 	$filePath32 = 'file     = (Join-Path $toolsDir "' + $filename32 + '")'
 
 	$obj.installScriptMod = '$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"' + "`n" + $obj.InstallScriptMod
 
 	$obj.installScriptMod = $obj.installScriptMod -replace "Install-ChocolateyPackage" , "Install-ChocolateyInstallPackage"
-	$obj.installScriptMod = $obj.installScriptMod -replace "packageArgs = @{" , "$&`n   $filePath32"
+	$obj.installScriptMod = $obj.installScriptMod -replace "= @{" , "$&`n   $filePath32"
 
 
 	download-fileSingle -url $url32 -filename $filename32 -toolsDir $obj.toolsDir
