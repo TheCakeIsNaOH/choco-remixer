47d27dde68814478341b11727da9ab6e6ec6ff5a
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-12-11 17:58:47 -0600

    Add dotnetcore-desktopruntime

--- PkgFunctions-special.ps1
+++ PkgFunctions-special.ps1
@@ -430,6 +430,31 @@ Function mod-vcredist140 ($obj) {
 }
 
 
+Function mod-dotnetcore-desktopruntime ($obj) {
+
+    $dataFile = Join-Path $obj.toolsDir 'data.ps1'
+    $dataContent = & $datafile
+    
+
+    $obj.installScriptMod = '$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"' + "`n" + $obj.InstallScriptMod
+    $obj.installScriptMod = $obj.installScriptMod -replace "Install-ChocolateyPackage" , "Install-ChocolateyInstallPackage"
+    $obj.installScriptMod = $obj.installScriptMod + "`n" + 'Remove-Item -Force -EA 0 -Path $toolsDir\*.exe'
+    
+    $url32 = $dataContent.Url
+    $url64 = $dataContent.Url64
+    $filename32 = ($url32 -split "/" | Select-Object -Last 1).tostring()
+    $filename64 = ($url64 -split "/" | Select-Object -Last 1).tostring()
+    $filePath32 = 'File    = (Join-Path $toolsDir "' + $filename32 + '")'
+    $filePath64 = 'File64   = (Join-Path $toolsDir "' + $filename64 + '")'
+
+
+    $obj.installScriptMod = $obj.installScriptMod -replace "arguments = @{" , "$&`n  $filePath32"
+    $obj.installScriptMod = $obj.installScriptMod -replace "arguments64 = @{" , "$&`n  $filePath64"
+
+    download-fileBoth -url32 $url32 -url64 $url64 -filename32 $filename32 -filename64 $filename64 -toolsDir $obj.toolsDir
+    
+}
+
 #Special because orig script uses $version instead of a full URL
 Function mod-powershell-core ($obj) {
     $version = $obj.version
