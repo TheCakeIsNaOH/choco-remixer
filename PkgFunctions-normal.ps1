a4048a74ce116831b4f6c60179fbd33d886177ea
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-12-16 17:09:34 -0600

    add yarn

--- PkgFunctions-normal.ps1
+++ PkgFunctions-normal.ps1
@@ -672,3 +672,20 @@ Function mod-makemkv ($obj) {
     download-fileSingle -url $url32 -filename $filename32 -toolsDir $obj.toolsDir
 }
 
+
+Function mod-yarn ($obj) {
+    $fullurl32 = ($obj.installScriptOrig -split "`n" | Select-String -pattern ' url ').tostring()
+    $url32 = ($fullurl32 -split "'" | Select-String -Pattern "http").tostring()
+    $filename32 = ($url32 -split "/" | Select-Object -Last 1).tostring()
+    $filePath32 = 'file     = (Join-Path $toolsDir "' + $filename32 + '")'
+
+    $obj.installScriptMod = '$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"' + "`n" + $obj.InstallScriptMod
+    $obj.installScriptMod = $obj.installScriptMod -replace "Install-ChocolateyPackage" , "Install-ChocolateyInstallPackage"
+    $obj.installScriptMod = $obj.installScriptMod -replace "packageArgs = @{" , "$&`n    $filePath32"
+    
+    $obj.installScriptMod = $obj.installScriptMod + "`n" + 'Remove-Item -Force -EA 0 -Path $toolsDir\*.msi'
+
+    download-fileSingle -url $url32 -filename $filename32 -toolsDir $obj.toolsDir
+}
+
+
