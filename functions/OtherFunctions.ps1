1262cde612938de38aa350b3f0da523ffae9b7ae
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-01-29 19:50:41 -0600

    Write install script as UTF8 with BOM on pwsh

--- functions/OtherFunctions.ps1
+++ functions/OtherFunctions.ps1
@@ -87,7 +87,13 @@ Function Write-UnzippedInstallScript {
     )
     (Get-ChildItem $toolsDir -Filter "*chocolateyinstall.ps1").fullname | ForEach-Object { Remove-Item -Force -Recurse -ea 0 -Path $_ } -ea 0
     $scriptPath = Join-Path $toolsDir 'chocolateyinstall.ps1'
-    $null = Out-File -FilePath $scriptPath -InputObject $installScriptMod -Force
+    
+    #If using pwsh, explicitly write with BOM
+    if ($PSVersionTable.PSVersion.major -ge 6) {
+        $null = Out-File -FilePath $scriptPath -InputObject $installScriptMod -Force -Encoding UTF8BOM
+    } else {
+        $null = Out-File -FilePath $scriptPath -InputObject $installScriptMod -Force -Encoding UTF8
+    }
 }
 
 
