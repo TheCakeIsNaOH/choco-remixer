8f90dff1cc8edfd421280aa643e148e9d5a05b8c
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-02-26 18:59:15 -0600

    Fix vscode launch files for module loading

---
+++ .vscode-functions/no-checks-no-save-no-pack.ps1
@@ -0,0 +1,3 @@
+﻿$moduleFile = [system.io.path]::Combine((Split-Path -Parent $PSScriptRoot), 'choco-remixer', 'choco-remixer.psm1')
+Import-Module $moduleFile -Force
+Invoke-InternalizeChocoPkg -skipRepoCheck -skipRepoMove -personalPkgXML personal-packages-testing.xml -nosave -nopack
