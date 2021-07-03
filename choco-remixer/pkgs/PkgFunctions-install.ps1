0f9a546f5f2e20de29c248243061b05736fe7ba7
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-07-03 12:12:02 -0500

    Add office365business

--- choco-remixer/pkgs/PkgFunctions-install.ps1
+++ choco-remixer/pkgs/PkgFunctions-install.ps1
@@ -96,6 +96,23 @@ Function Convert-geogebra-classic ($obj) {
     Get-File -url $url32 -filename $filename32 -toolsDir $obj.toolsDir -checksumTypeType 'sha256' -checksum $checksum32
 }
 
+Function Convert-office365business ($obj) {
+    $fullurl32 = ($obj.installScriptOrig -split "`n" | Select-String -Pattern '\surl\s+=').tostring()
+    $url32 = ($fullurl32 -split "'" | Select-String -Pattern "http").tostring()
+    $filename32 = ($url32 -split "/" | Select-Object -Last 1).tostring()
+    $filePath32 = 'file     = (Join-Path $toolsDir "' + $filename32 + '")'
+
+
+    $obj.installScriptMod = '$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"' + "`n" + $obj.InstallScriptMod
+    $obj.installScriptMod = '$ErrorActionPreference = ''Stop''' + "`n" + $obj.InstallScriptMod
+    $obj.installScriptMod = $obj.installScriptMod + "`n" + 'Remove-Item -Force -EA 0 -Path $toolsDir\*.exe'
+    $obj.installScriptMod = $obj.installScriptMod -replace "\sInstall-ChocolateyPackage" , "Install-ChocolateyInstallPackage"
+    $obj.installScriptMod = $obj.installScriptMod -replace "packageArgs\s+=\s+@{" , "$&`n    $filePath32"
+
+    $checksum32 = ($obj.installScriptOrig -split "`n" | Select-String -Pattern '\s+Checksum\s+=').tostring() -split "'" | Select-Object -Last 1 -Skip 1
+    Get-File -url $url32 -filename $filename32 -toolsDir $obj.toolsDir -checksumTypeType 'sha256' -checksum $checksum32
+}
+
 
 Function Convert-anydesk-install ($obj) {
     $fullurl32 = ($obj.installScriptOrig -split "`n" | Select-String -Pattern '^\$Url32 ').tostring()
