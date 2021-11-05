f5d66a405b13796ed3e49c4170f1ad992aea8b7c
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2021-11-05 18:43:09 -0500

    Repomove fix save path

--- choco-remixer/public/Invoke-RepoMove.ps1
+++ choco-remixer/public/Invoke-RepoMove.ps1
@@ -119,7 +119,7 @@
                         $downloadURL = $searchResults.items.assets.downloadURL
                         $downloadChecksum = $searchResults.items.assets.checksum.sha512
 
-                        Get-File -url $downloadURL -filename $filename -toolsDir $saveDir -checksum $downloadChecksum -checksumTypeType 'sha512' -authorization "Basic $proxyRepoCredsBase64"
+                        Get-File -url $downloadURL -filename $filename -toolsDir $IdSaveDir -checksum $downloadChecksum -checksumTypeType 'sha512' -authorization "Basic $proxyRepoCredsBase64"
 
                         Write-Information "$nuspecID $_ found and downloaded, will be deleted next run if internalization succeeds" -InformationAction Continue
                     }
